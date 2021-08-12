bfd3694493c950165d1a0ee040ffba1d85ac06b1
sivaramanshira <34826837+sivaramanshira@users.noreply.github.com>
2021-08-12 10:35:57 +0530

    Update source.py

--- source.py
+++ source.py
@@ -12,7 +12,7 @@ class TwitterClient(object):
 
         # keys and tokens from the Twitter Dev Console
         consumer_key = '#####################'
-        consumer_secret = '########################'
+        consumer_secret = '#######################'
         access_token = '#########################'
         access_token_secret = '#################################'
   
